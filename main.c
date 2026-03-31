b0e2a01fd3e608ce801de22db6b92c112a8f906a
Joaquin Negrete <joaquin.negrete03@gmail.com>
2026-03-31 11:53:26 +0200

    Format code

--- main.c
+++ main.c
@@ -4,12 +4,13 @@
 #define MINIGIT_NAME "minigit"
 
 int main() {
-    while(true) {
+    while (true) {
         char **args = NULL;
         int argc = parseMiniGitArgs(&args);
         if (argc == -1) {
             break;
         }
+
         if (argc == 0) {
             free(args);
             continue;
@@ -17,18 +18,27 @@ int main() {
 
         // Check if we are in front of a minigit command...
         // If we are not, just echo and go to the next iteration
-        if(strncmp(args[0], MINIGIT_NAME, strlen(MINIGIT_NAME)) != 0) {
-            for(int i=0; i < argc; i++) { fprintf(stdout, "%s", args[i]); printf(" "); }
+        if (strncmp(args[0], MINIGIT_NAME, strlen(MINIGIT_NAME)) != 0) {
+            for (int i = 0; i < argc; i++) {
+                fprintf(stdout, "%s", args[i]);
+                printf(" ");
+            }
             printf("\n");
             free(args);
             continue;
         }
 
         // If we are, we now have to check which command we are running
-        if(argc == 1) { printf("Run 'minigit --help' for help with the use of minigit\n"); continue; }
-        if(strcmp(args[1], "init") == 0) { initRepo(); }
+        if (argc == 1) {
+            printf("Run 'minigit --help' for help with the use of minigit\n");
+            continue;
+        }
 
-        // Always free when done 
+        if (strcmp(args[1], "init") == 0) {
+            initRepo();
+        }
+
+        // Always free when done
         free(args);
     }
 
