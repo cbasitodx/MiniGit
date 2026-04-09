86e53e100141644eaabe501a1e642e35027525ce
Joaquin Negrete <joaquin.negrete03@gmail.com>
2026-04-09 21:23:53 +0200

    Implement an error passing API between back and front end

---
+++ include/utils/errors.h
@@ -0,0 +1,20 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+
+#include <stdarg.h>
+
+#define MG_SUCCESS 0
+#define MG_ERR_NOT_ENOUGH_ARGS 1
+#define MG_ERR_TOO_MANY_ARGS 2
+#define MG_ERR_UNKNOWN_OPTION 3
+#define MG_ERR_ALLOCATION_FAILED 4
+#define MG_ERR_FILE_OPEN_FAILED 5
+
+typedef struct {
+    int code;
+    char message[512];
+} mg_error_t;
+
+int mg_set_error(mg_error_t *err, int code, const char *fmt, ...);
+
+#endif /* ERRORS_H */
---
+++ src/utils/errors.c
@@ -0,0 +1,15 @@
+#include "utils/errors.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int mg_set_error(mg_error_t *err, int code, const char *fmt, ...) {
+    err->code = code;
+
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(err->message, sizeof(err->message), fmt, args);
+    va_end(args);
+
+    return code;
+}
