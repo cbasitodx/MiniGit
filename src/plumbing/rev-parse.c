325e69e1249c04222dffd255277279cd4fb21af4
Joaquin Negrete <joaquin.negrete03@gmail.com>
2026-04-10 15:50:55 +0200

    Format code

--- src/plumbing/rev-parse.c
+++ src/plumbing/rev-parse.c
@@ -1,26 +1,29 @@
 #include "plumbing/rev-parse.h"
 
-#include "utils/errors.h"
-
 #include <stdbool.h>
 #include <string.h>
 
+#include "utils/errors.h"
+
 // TODO: This should agree with the one in hash-content.h,
-// find an idiomatic solution without importing hash-content.h??? 
+// find an idiomatic solution without importing hash-content.h???
 #define SHA1_HEX_LENGTH 40
 
 // TODO: Add more features. Adding more features means adding a variable number of arguments
 #define REV_PARSE_MAX_ARGS 1
 
-bool isValidHash(const char *str) {                                                    
-    if (strlen(str) != SHA1_HEX_LENGTH) return false;                                               
+bool isValidHash(const char *str) {
+    if (strlen(str) != SHA1_HEX_LENGTH) {
+        return false;
+    }
     for (int i = 0; i < SHA1_HEX_LENGTH; i++) {
-        if (!((str[i] >= '0' && str[i] <= '9') ||                                      
-            (str[i] >= 'a' && str[i] <= 'f')))
-            return false;                                                              
-    }           
-    return true;                                                                       
-}       
+        if (!((str[i] >= '0' && str[i] <= '9') ||
+              (str[i] >= 'a' && str[i] <= 'f'))) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int handleRevParseArgsFromCLI(int argc, char **args_in, RevParseArgs *args_out, mg_error_t *err) {
     if (argc < REV_PARSE_MAX_ARGS + 2) {
@@ -45,9 +48,8 @@ int handleRevParseArgsFromCLI(int argc, char **args_in, RevParseArgs *args_out, 
 }
 
 int revParse(RevParseArgs *args, mg_error_t *err) {
-    if(isValidHash(args->rev_name)) {
-
+    if (isValidHash(args->rev_name)) {
     }
-    
+
     return MG_SUCCESS;
 }
